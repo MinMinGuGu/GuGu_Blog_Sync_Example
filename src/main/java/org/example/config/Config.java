e03f37f71eed294602ac10d7a3f55a62876f6249
MinMin <newmailbyminmin@gmail.com>
2023-03-18 05:50:58 +0800

    添加默认配置

--- src/main/java/org/example/config/Config.java
+++ src/main/java/org/example/config/Config.java
@@ -1,5 +1,7 @@
 package org.example.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +11,10 @@ import org.springframework.context.annotation.Configuration;
  * @author minmin
  * @date 2023 /03/08
  */
+@Setter
+@Getter
 @Configuration
-@ConfigurationProperties("example.config")
+@ConfigurationProperties("example-config")
 public class Config {
+    private String value;
 }
